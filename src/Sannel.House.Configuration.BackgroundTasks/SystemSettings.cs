8e97e6b60466aa25033a95ab39b32d27a6c24e03
Adam Holt <holtsoftware@outlook.com>
2017-07-04 13:40:07 -0600

    Added Front end for configuring.

---
+++ src/Sannel.House.Configuration.BackgroundTasks/SystemSettings.cs
@@ -0,0 +1,40 @@
+﻿using Sannel.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sannel.House.Configuration.BackgroundTasks
+{
+	internal class SystemSettings : SettingsBase, ISystemSettings
+	{
+		public override void Initialize()
+		{
+			Init(this);
+		}
+		public string ServerApiUrl
+		{
+			get => GetValue();
+			set => SetValue(value);
+		}
+
+		public string ServerUsername
+		{
+			get => GetValue();
+			set => SetValue(value);
+		}
+
+		public string ServerPassword
+		{
+			get => GetValue();
+			set => SetValue(value);
+		}
+
+		public int SensorsPort
+		{
+			get => GetValue<int>();
+			set => SetValue(value);
+		}
+	}
+}
---
+++ src/Sannel.House.Configuration/SystemSettings.cs
@@ -0,0 +1,46 @@
+﻿using Sannel.Configuration;
+using Sannel.House.Configuration.BackgroundTasks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sannel.House.Configuration
+{
+	internal class SystemSettings : SettingsBase, ISystemSettings
+	{
+
+		public override void Initialize()
+		{
+			Init(this);
+		}
+		[SettingsProperty("Server Api Url", SettingsPropertyType.Uri)]
+		public string ServerApiUrl
+		{
+			get => GetValue();
+			set => CheckAndSetUri(value, "Server Api Url is invalid");
+		}
+
+		[SettingsProperty("Server Username", SettingsPropertyType.String)]
+		public string ServerUsername
+		{
+			get => GetValue();
+			set => SetValue(value);
+		}
+
+		[SettingsProperty("Server Password", SettingsPropertyType.Password)]
+		public string ServerPassword
+		{
+			get => GetValue();
+			set => SetValue(value);
+		}
+
+		[SettingsProperty("Sensor Capture Port", SettingsPropertyType.Integer)]
+		public int SensorsPort
+		{
+			get => GetValue(defaultValue: 8175);
+			set => SetValue(value);
+		}
+	}
+}
