bee8420f49b9b1c725e0096264d3ca53776e651d
Adam Holt <holtsoftware@outlook.com>
2018-04-08 12:10:02 -0600

    Updated post build script to publish

--- appcenter-post-build.ps1
+++ appcenter-post-build.ps1
@@ -9,3 +9,6 @@ $xml.package.metadata.version = $version
 $xml.Save($path);
 
 nuget pack $path
+nuget setapikey $env:MYGET_APIKEY -Source "https://www.myget.org/F/sannel-house/api/v3/index.json"
+$package = ".\Sannel.House.Configuration." + $version + ".nupkg"
+nuget push $package -Source "https://www.myget.org/F/sannel-house/api/v3/index.json"
