b22f2e219e5f5d38d0b8a5cee5535afb6dbe5eeb
Adam Holt <holtsoftware@outlook.com>
2018-04-08 11:19:57 -0600

    Trying to get the build service to also build the nuget package.

---
+++ appcenter-post-build.ps1
@@ -0,0 +1,11 @@
+$version = $env:Version + $env:APPCENTER_BUILD_ID;
+if($env:APPCENTER_BRANCH -ne "master")
+{
+	$version = $version + "-alpha";
+}
+$path = ".\nuspec\Sannel.House.Configuration.nuspec"
+[xml]$xml = Get-Content $path
+$xml.package.metadata.version = $version
+$xml.Save($path);
+
+nuget pack $path
